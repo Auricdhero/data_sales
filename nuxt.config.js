8c35b697675cd82f1ae3c823be682f6dd5f9b7b7
zakie07 <emmanuelprah51@gmail.com>
2023-03-03 09:07:45 +0000

    pull out

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    ["~/plugins/supabaseclients.ts"],
+    ["~/plugins/supabaseClient.js"],
     '~/plugins/commerce.js'
   ],
 
---
+++ pages/testData.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const reports = ref([])
+
+async function getCountries() {
+
+    let { data: reports, error } = await supabase
+        .from('reports')
+        .select('*')
+    reports.value = data
+}
+
+onMounted(() => {
+    getCountries()
+})
+    < ul >
+    <li v-for="report in reports" : key="report.id">{{ report.name }}</li>
+</ul > <ul>
+    <li v-for="report in reports" :key="country.id">{{ report.name }}</li>
+  </ul >
+</template >
